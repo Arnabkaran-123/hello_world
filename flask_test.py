cd39e83201da2ae9e84187f4e420d0f581f97829
Jyotiprakashsky <jyoti.p.80386@gmail.com>
2022-01-01 12:19:13 +0530

    static login

--- flask_test.py
+++ flask_test.py
@@ -4,20 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return render_template('index.html')
 
-#app.run(host='0.0.0.0', port=8080)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        #db validation here
+        if request.form['user_name'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return render_template('home.html')
-    return render_template('login.html', error=error)
+            return render_template('document.html')
+    return render_template('index.html', error=error)
 
 @app.route('/home', methods=['GET', 'POST'])
 def homelink():
     error = None
     return render_template('home.html')
+
+app.run(host='0.0.0.0', port=8080)
